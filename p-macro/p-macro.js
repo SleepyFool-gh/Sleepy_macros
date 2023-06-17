1309840bd410f76f850e8590a1a80a9d59bc0c1a
SleepyFool <sleepy.fool.gm@gmail.com>
2023-06-17 11:51:39 -0700

    moved aux functions to macro properties

--- p-macro/p-macro.js
+++ p-macro/p-macro.js
@@ -2,7 +2,6 @@ Macro.add('p', {
     tags: null,
     handler() {
         
-        
         // much thanks to Hituro & Sjoerd who helped clean, improve & troubleshoot
 
         // check if custom p delimiter, set to default if no
@@ -10,6 +9,8 @@ Macro.add('p', {
 
         const frag = document.createDocumentFragment();
         const contents = this.payload[0].contents.trim();
+        const postProcess = (pId) => this.self.postProcess(pId);
+
 
         // count macro instance, set id
         this.self.count = (this.self.count || 0) + 1;
@@ -31,59 +32,61 @@ Macro.add('p', {
 
         $(this.output).append(frag);
 
-        setTimeout( function() {
-            postProcess(pId)
-        }, 0);
-
-        
-        // aux function, post process
-        const postProcess = function(pId) {
-            const wout = $(`#p-macro-output-${pId}`).contents();
-            const toWrap = [];
-
-            // if current node is a text node or inline element that is not the break or a br, initiate wrap queuer
-            for (let i = 0; i < wout.length - 1; i++) {
-                const currentNode = wout[i];
-                if (currentNode.nodeType === Node.TEXT_NODE || (currentNode.nodeType === Node.ELEMENT_NODE && !['BR','P-MACRO-BREAK'].includes(currentNode.nodeName) && window.getComputedStyle(currentNode).display.includes('inline'))) {
-                    const endIndex = wrapUntil(i, pId);
-                    toWrap.push([i, endIndex]);
-                    i = endIndex;
-                }
-            }
-
-            // wrap nodes with queue, starting from end, backwards
-            for (let k = toWrap.length - 1; k >= 0; k--) {
-                const [startIdx, endIdx] = toWrap[k];
-                wout.slice(startIdx, endIdx + 1).wrapAll('<p class="p-macro"></p>');
-            }
-
-            // remove breaks's and unwrap
-            $(`#p-macro-output-${pId}`).find('p-macro-break').remove();
-            $(`#p-macro-output-${pId}`).contents().unwrap();
-        }
-
-        // aux function, wrap queuer
-        const wrapUntil = function(startIndex, pId) {
-            const wout = $(`#p-macro-output-${pId}`).contents();
-            let endIndex = startIndex;
-
-            // checks next nodes until it finds either a break or something not a text node and not an inline element
-            while (endIndex + 1 <= wout.length - 1) {
-                const nextNode = wout[endIndex + 1];
-
-                if (nextNode.nodeName === 'P-MACRO-BREAK') {
-                    endIndex += 1;
-                    break;
-                } else if (nextNode.nodeType === Node.TEXT_NODE || window.getComputedStyle(nextNode).display.includes('inline')) {
-                    endIndex += 1;
-                } else {
-                    break;
-                }
-            }
-
-            return endIndex;
-        }
-        
+        setTimeout(() => postProcess(pId), 0);
 
     },
+
+
+
+    // aux function, post process, wraps after wiki
+    postProcess: function(pId) {
+        const wout = $(`#p-macro-output-${pId}`).contents();
+        const toWrap = [];
+        const wrapUntil = (startIndex, pId) => this.wrapUntil(startIndex,pId);
+
+        // if current node is a text node or inline element that is not the break or a br, initiate wrap queuer
+        for (let i = 0; i < wout.length - 1; i++) {
+            const currentNode = wout[i];
+            if (currentNode.nodeType === Node.TEXT_NODE || (currentNode.nodeType === Node.ELEMENT_NODE && !['BR','P-MACRO-BREAK'].includes(currentNode.nodeName) && window.getComputedStyle(currentNode).display.includes('inline'))) {
+                const endIndex = wrapUntil(i, pId);
+                toWrap.push([i, endIndex]);
+                i = endIndex;
+            }
+        }
+
+        // // wrap nodes with queue, starting from end, backwards
+        for (let k = toWrap.length - 1; k >= 0; k--) {
+            const [startIdx, endIdx] = toWrap[k];
+            wout.slice(startIdx, endIdx + 1).wrapAll('<p class="p-macro"></p>');
+        }
+
+        // remove breaks's and unwrap
+        $(`#p-macro-output-${pId}`).find('p-macro-break').remove();
+        $(`#p-macro-output-${pId}`).contents().unwrap();
+
+    },
+
+    // aux function, wrap queuer, checks adjacent nodes to know which to wrap together
+    wrapUntil: function(startIndex, pId) {
+        const wout = $(`#p-macro-output-${pId}`).contents();
+        let endIndex = startIndex;
+
+        // checks next nodes until it finds either a break or something not a text node and not an inline element
+        while (endIndex + 1 <= wout.length - 1) {
+            const nextNode = wout[endIndex + 1];
+
+            if (nextNode.nodeName === 'P-MACRO-BREAK') {
+                endIndex += 1;
+                break;
+            } else if (nextNode.nodeType === Node.TEXT_NODE || window.getComputedStyle(nextNode).display.includes('inline')) {
+                endIndex += 1;
+            } else {
+                break;
+            }
+        }
+
+        return endIndex;
+
+    }
+
 });
